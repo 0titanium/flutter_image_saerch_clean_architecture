e7b145c60c80d3645dfefa2070c8e6b3638e0a61
kimhyungmin <jeen14rla@gmail.com>
2024-04-17 16:51:08 +0900

    add: add ChangeNotifierProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_image_saerch_clean_architecture/data/data_source/photo_api.dart';
+import 'package:flutter_image_saerch_clean_architecture/data/repository/photo_repository_impl.dart';
+import 'package:flutter_image_saerch_clean_architecture/domain/use_case/get_photos_use_case.dart';
 import 'package:flutter_image_saerch_clean_architecture/presentation/search_list/search_list_screen.dart';
+import 'package:flutter_image_saerch_clean_architecture/presentation/search_list/search_list_view_model.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,15 +13,24 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-    @override
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-                                                                                                                                colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const SearchListScreen(),
+      home: ChangeNotifierProvider(
+        create: (_) => SearchListViewModel(
+          getPhotosUseCase: GetPhotosUseCase(
+            PhotoRepositoryImpl(
+              PhotoApi(),
+            ),
+          ),
+        ),
+        child: const SearchListScreen(),
+      ),
     );
   }
 }
