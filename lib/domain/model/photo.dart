0480bd50f236338f2fbe61594c59f47d50e24892
kimhyungmin <jeen14rla@gmail.com>
2024-04-17 15:03:42 +0900

    feat: add photo model

---
+++ lib/domain/model/photo.dart
@@ -0,0 +1,17 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'photo.freezed.dart';
+
+part 'photo.g.dart';
+
+@freezed
+class Photo with _$Photo {
+  const factory Photo({
+    required int id,
+required String url,
+required String largeImageURL,
+    required String tags,
+  }) = _Photo;
+  
+  factory Photo.fromJson(Map<String, Object?> json) => _$PhotoFromJson(json); 
+}
