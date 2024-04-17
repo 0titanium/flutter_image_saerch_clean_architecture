ca544d51f6581258414092a0d69234042021e16a
kimhyungmin <jeen14rla@gmail.com>
2024-04-17 16:50:49 +0900

    feat: add photo_detail_screen

---
+++ lib/presentation/photo_detail/photo_detail_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_image_saerch_clean_architecture/domain/model/photo.dart';
+
+class PhotoDetailScreen extends StatelessWidget {
+  final Photo photo;
+
+  const PhotoDetailScreen({super.key, required this.photo});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(photo.tags),
+      ),
+      body: Hero(
+        tag: photo.id,
+        child: Image.network(
+          photo.largeImageUrl,
+        ),
+      ),
+    );
+  }
+}
